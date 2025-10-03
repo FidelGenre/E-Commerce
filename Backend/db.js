509477de320c3aed56c7f2863a1c90db917051bc
Fidel Genre <trabajosfidel4@gmail.com>
2025-10-03 06:19:43 -0300

    aaa

--- Backend/db.js
+++ Backend/db.js
@@ -5,11 +5,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
